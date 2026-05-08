38178e0de7635bdf3118497fa4c1686be81eb782
regnar0319 <maheshchad0319@gmail.com>
2026-05-08 18:40:30 +0530

    module fixes

--- src/llm.py
+++ src/llm.py
@@ -1,7 +1,3 @@
-from langchain_core.prompts import PromptTemplate
-from langchain_community.llms import Ollama
-from typer import prompt
-
 def get_chain():
     class Dummy:
         def invoke(self, x):
